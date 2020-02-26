c11b337ae21112c3fa310aee5566d7befa8d16bb
DianaSamusenko <dianas190999@gmail.com>
2020-02-26 09:56:18 +0300

    Initial Commit

---
+++ PhoneVerification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhoneVerification
+//
+//  Created by Diana Samusenka on 2/26/20.
+//  Copyright © 2020 Diana Samusenka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
